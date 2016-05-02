79829a6a67ae5a19febda78cd4247e8ff2b01a85
eguegan <e.guegan@margulls.com>
2016-05-02 14:46:13 -0400

    first commit

---
+++ app/src/main/java/com/example/admin/w5d2mytravis/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.admin.w5d2mytravis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
